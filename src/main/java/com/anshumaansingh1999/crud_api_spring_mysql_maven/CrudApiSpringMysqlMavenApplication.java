114df8739580a2c54c84a1ef3860c2ba832cb5b9
Anshumaan Singh <anshumaanborntolead@gmail.com>
2024-08-02 20:53:49 +0530

    Initial commit

---
+++ src/main/java/com/anshumaansingh1999/crud_api_spring_mysql_maven/CrudApiSpringMysqlMavenApplication.java
@@ -0,0 +1,13 @@
+package com.anshumaansingh1999.crud_api_spring_mysql_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiSpringMysqlMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiSpringMysqlMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anshumaansingh1999/crud_api_spring_mysql_maven/CrudApiSpringMysqlMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshumaansingh1999.crud_api_spring_mysql_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiSpringMysqlMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
